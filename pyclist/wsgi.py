3ce1d223a65be49b274607c84b457ba10f81bc31
Aleksey Ropan <nap0rbl4@gmail.com>
2019-05-30 02:24:25 +0300

    Added collectstatic on run wsgi

--- pyclist/wsgi.py
+++ pyclist/wsgi.py
@@ -8,7 +8,15 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pyclist.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pyclist.settings")  # noqa
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
+from django import setup
+from django.conf import settings
+
+if not settings.DEBUG:
+    setup()
+    call_command('collectstatic', verbosity=1, interactive=False)
+
 application = get_wsgi_application()
