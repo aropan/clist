f48a1e333f30be0d37558e0ab1b48c620db96c66
Aleksey Ropan <nap0rbl4@gmail.com>
2020-02-10 22:57:54 +0300

    Use pyclist/conf.py instead .pgpass

--- requirements/configure.sh
+++ requirements/configure.sh
@@ -4,18 +4,15 @@ set -e -x
 
 wget -O /etc/bash_completion.d/django_bash_completion.sh https://raw.github.com/django/django/master/extras/django_bash_completion
 
+while IFS= read -r line; do
+    export $line
+done <<< $(cat "$(dirname $BASH_SOURCE[0])/../pyclist/conf.py" | grep " = " | sed "s/ = '\?/=/;s/'$//")
+
 mkdir -p /var/log/nginx/clist/
 mkdir -p /var/log/nginx/clist/legacy/
 
+sudo -u postgres createuser $DB_USER || :
+sudo -u postgres createdb $DB_NAME || :
 
-pgdata=($(cat $(dirname $BASH_SOURCE[0])/../.pgpass | tr ":" " "))
-
-PGUSER=${pgdata[3]}
-PGPASSWORD=${pgdata[4]}
-PGDATABASE=${pgdata[2]}
-
-sudo -u postgres createuser $PGUSER || :
-sudo -u postgres createdb $PGDATABASE || :
-
-sudo -u postgres psql -c "alter user $PGUSER with encrypted password '$PGPASSWORD';"
-sudo -u postgres psql -c "grant all privileges on database $PGDATABASE to $PGUSER;"
+sudo -u postgres psql -c "alter user $DB_USER with encrypted password '$DB_PASSWORD';"
+sudo -u postgres psql -c "grant all privileges on database $DB_NAME to $DB_USER;"
