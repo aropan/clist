32ba9451478b2791b2c254e84dd9185682711970
Aleksey Ropan <nap0rbl4@gmail.com>
2020-02-29 00:35:20 +0300

    Added college contests hackerrank

--- legacy/module/hackerrank.com/index.php
+++ legacy/module/hackerrank.com/index.php
@@ -8,25 +8,22 @@
     if (!isset($TIMEZONE)) $TIMEZONE = 'UTC';
     if (!isset($contests)) $contests = array();
 
-    $page = curlexec($URL);
-
-    if (!preg_match("/^\{.*\}$/m", $page, $match)) {
-        return;
-    }
-    $json = json_decode($match[0], true);
-
-    foreach ($json['models'] as $model)
-    {
-        $contests[] = array(
-            'start_time' => date('r', $model['epoch_starttime']),
-            'end_time' => date('r', $model['epoch_endtime']),
-            'title' => $model['name'],
-            'url' => 'https://www.hackerrank.com/contests/' . $model['slug'],
-            'host' => $HOST,
-            'rid' => $RID,
-            'timezone' => 'UTC',
-            'key' => $model['id']
-        );
+    $urls = array($URL, 'https://www.hackerrank.com/rest/contests/college');
+    foreach ($urls as $url) {
+        $json = curlexec($url, NULL, array('json_output' => true));
+        foreach ($json['models'] as $model)
+        {
+            $contests[] = array(
+                'start_time' => date('r', $model['epoch_starttime']),
+                'end_time' => date('r', $model['epoch_endtime']),
+                'title' => $model['name'],
+                'url' => 'https://www.hackerrank.com/contests/' . $model['slug'],
+                'host' => $HOST,
+                'rid' => $RID,
+                'timezone' => 'UTC',
+                'key' => $model['id']
+            );
+        }
     }
     if ($RID == -1) {
         print_r($contests);
