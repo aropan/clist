f767a9b03e93e89d57562b43ff18b2f3a55a7b71
Aleksey Ropan <nap0rbl4@gmail.com>
2020-03-02 00:58:33 +0300

    Added codility.com

---
+++ legacy/module/codility.com/index.php
@@ -0,0 +1,63 @@
+<?php
+    require_once dirname(__FILE__) . "/../../config.php";
+
+    if (!isset($URL)) $URL = "https://app.codility.com/programmers/challenges/";
+    if (!isset($HOST)) $HOST = parse_url($URL, PHP_URL_HOST);
+    if (!isset($RID)) $RID = -1;
+    if (!isset($LANG)) $LANG = 'RU';
+    if (!isset($TIMEZONE)) $TIMEZONE = 'UTC';
+    if (!isset($contests)) $contests = array();
+
+    $parse_full_list = isset($_GET['parse_full_list']);
+
+    $page = curlexec($URL);
+
+    if (!preg_match_all('#<a[^>]*href="(?P<url>/programmers/challenges/(?P<key>[^/]*)/?)"[^>]*>#', $page, $matches, PREG_SET_ORDER)) {
+        return;
+    }
+
+    $timedelta_day = 24 * 60 * 60;
+    $timedelta_week = 7 * $timedelta_day;
+    foreach ($matches as $c) {
+        $url = $c['url'];
+        $page = curlexec($url);
+
+        if (!preg_match('#<div[^>]*class="header"[^>]*>[^<]*<div[^>]*>[^<]*<h2[^>]*>(?P<title>[^<]*)</h2>[^/]*>\s*Start date:\s*(?P<start_time>[^<]*)#i', $page, $match)) {
+            return;
+        }
+        $start_challenge = boolval(preg_match('#START CHALLENGE#', $page));
+
+        $start_time = strtotime(preg_replace('#[[:space:]]+#', ' ', $match['start_time']));
+        $title = trim($match['title']);
+
+        $now = time();
+        $end_time = max($now + $timedelta_week, $start_time + 3 * $timedelta_week);
+        if (!$start_challenge) {
+            $end_time = min($end_time, $now);
+        }
+        $end_time = intdiv(($end_time - $start_time), $timedelta_week) * $timedelta_week + $start_time;
+        if (!$start_challenge && isset($prev_start_time)) {
+            $end_time = min($end_time, $prev_start_time - $timedelta_day);
+        }
+        $prev_start_time = $start_time;
+
+        $contests[] = array(
+            'start_time' => $start_time,
+            'end_time' => $end_time,
+            'title' => $title,
+            'url' => $url,
+            'host' => $HOST,
+            'rid' => $RID,
+            'timezone' => $TIMEZONE,
+            'key' => $c['key'],
+        );
+
+        if (!$parse_full_list) {
+            break;
+        }
+    }
+
+    if ($RID === -1) {
+        print_r($contests);
+    }
+?>
