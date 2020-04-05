ae17ef205f3c0bd68727a901970322759c92a95e
Aleksey Ropan <nap0rbl4@gmail.com>
2020-04-06 01:22:52 +0300

    Added participants filters

--- events/admin.py
+++ events/admin.py
@@ -4,12 +4,12 @@ import time
 from django.contrib import admin
 from django.http import HttpResponse
 from django.utils import timezone
-from django.db.models import OuterRef, Exists
+from django.db.models import Q, OuterRef, Exists
 from django_print_sql import print_sql_decorator
 
 
 from pyclist.admin import BaseModelAdmin, admin_register
-from events.models import Event, Participant, JoinRequest, Team, Login
+from events.models import Event, Participant, JoinRequest, Team, Login, TeamStatus
 
 
 @admin_register(Event)
@@ -36,7 +36,61 @@ class ParticipantAdmin(BaseModelAdmin):
         'email',
         'is_coach',
     ]
-    list_filter = ['event', 'is_coach', 'organization', 'country']
+
+    class HasTeamFilter(admin.SimpleListFilter):
+        title = 'has registered team'
+        parameter_name = 'has_registered_team'
+
+        def lookups(self, request, model_admin):
+            return (
+                ('yes', 'Yes'),
+                ('no', 'No'),
+            )
+
+        def queryset(self, request, queryset):
+            if self.value() in ['yes', 'no']:
+                new_statuses = [k for k, v in TeamStatus.descriptions.items() if v == 'new']
+                cond = Q()
+                for status in new_statuses:
+                    cond |= Q(team__status=status)
+                cond |= Q(team__isnull=True)
+                if self.value() == 'yes':
+                    queryset = queryset.exclude(cond)
+                else:
+                    queryset = queryset.filter(cond)
+            return queryset
+
+    list_filter = ['event', 'is_coach', HasTeamFilter, 'organization', 'country']
+
+    @print_sql_decorator(count_only=True)
+    def import_to_csv(self, request, queryset):
+        queryset = queryset.select_related('team')
+        filename = 'participants-{}.csv'.format(timezone.now().strftime('%Y%m%d%H%M'))
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=%s' % filename
+        encoding = 'cp1251' if request.user_agent.os.family == 'Windows' else 'utf8'
+        writer = csv.writer(response, delimiter=';', encoding=encoding, errors='replace')
+        fields = [
+            'email',
+            'first_name',
+            'last_name',
+            'first_name_native',
+            'last_name_native',
+            'middle_name_native',
+            'phone_number',
+            'organization',
+            'country',
+            'tshirt_size_value',
+            'is_coach',
+        ]
+        writer.writerow(['title'] + [f.replace('_', ' ').title() for f in fields])
+        for member in queryset:
+            values = [member.team.name if member.team else ''] + [str(getattr(member, f)) for f in fields]
+            writer.writerow(values)
+        return response
+    import_to_csv.short_description = 'Import to csv'
+
+    actions = [import_to_csv]
 
 
 @admin_register(JoinRequest)
@@ -88,7 +142,7 @@ class TeamAdmin(BaseModelAdmin):
 
     @print_sql_decorator(count_only=True)
     def import_to_csv(self, request, queryset):
-        filename = 'information-{}.csv'.format(timezone.now().strftime('%Y%m%d%H%M'))
+        filename = 'teams-{}.csv'.format(timezone.now().strftime('%Y%m%d%H%M'))
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename=%s' % filename
         encoding = 'cp1251' if request.user_agent.os.family == 'Windows' else 'utf8'
