7a8d76978ef8bfe5f3a6fd9f56c1eb6d96187f82
Aleksey Ropan <nap0rbl4@gmail.com>
2020-04-09 21:55:12 +0300

    Event mailing to participants

--- events/management/commands/event_mailing.py
+++ events/management/commands/event_mailing.py
@@ -6,6 +6,7 @@ from django.template.loader import get_template
 from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
 from datetime import timedelta
+from true_coders.models import Coder
 from events.models import Team, Event, TeamStatus
 
 import tqdm
@@ -17,11 +18,12 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--event', type=str, required=True)
         parser.add_argument('--status', type=str, required=True)
+        parser.add_argument('--each', action='store_true', default=False)
         parser.add_argument('--dryrun', action='store_true', default=False)
         parser.add_argument('--template', type=str, required=True)
 
     def handle(self, *args, **options):
-        event = Event.objects.filter(name__regex=options['event']).order_by('-created').first()
+        event = Event.objects.filter(name__iregex=options['event']).order_by('-created').first()
         self.stdout.write(f'event = {event}')
         template = get_template(options['template'])
         status = getattr(TeamStatus, options['status'].upper())
@@ -40,30 +42,46 @@ class Command(BaseCommand):
 
         with event.email_backend() as connection:
             logins = [team.login_set.get(stage=status) for team in teams]
-            for team, login in tqdm.tqdm(zip(teams, logins), total=len(logins)):
-                message = template.render({'team': team, 'login': login})
-                subject, message = message.split('\n\n', 1)
-                message = message.replace('\n', '<br>\n')
-                to = [settings.ADMINS[0][1]] if options['dryrun'] else [m.email for m in team.ordered_participants]
+            with tqdm.tqdm(zip(teams, logins), total=len(logins)) as pbar:
+                for team, login in pbar:
+                    context = {'team': team, 'login': login}
+                    jobs = []
+                    if options['dryrun']:
+                        email = settings.ADMINS[0][1]
+                        jobs = [{'email': [email], 'coder': Coder.objects.get(username='aropan')}]
+                    else:
+                        if options['each']:
+                            jobs = [{'email': [p.email], 'coder': p.coder} for p in team.ordered_participants]
+                        else:
+                            jobs = [{'email': [p.email for p in team.ordered_participants]}]
 
-                msg = EmailMultiAlternatives(
-                    subject,
-                    message,
-                    to=to,
-                    connection=connection,
-                )
-                msg.attach_alternative(message, 'text/html')
+                    for job in jobs:
+                        context.update(job)
+                        message = template.render(context)
+                        subject, message = message.split('\n\n', 1)
+                        message = message.replace('\n', '<br>\n')
+                        msg = EmailMultiAlternatives(
+                            subject,
+                            message,
+                            to=job['email'],
+                            connection=connection,
+                        )
+                        msg.attach_alternative(message, 'text/html')
 
-                for i in range(n_attempet):
-                    result = msg.send()
-                    if result:
-                        done += 1
-                        time.sleep(time_wait_on_success)
-                        break
-                    time.sleep(time_wait_on_failed)
-                else:
-                    failed += 1
-                if options['dryrun']:
-                    break
+                        for i in range(n_attempet):
+                            try:
+                                result = msg.send()
+                                if result:
+                                    done += 1
+                                    time.sleep(time_wait_on_success)
+                                    break
+                            except Exception as e:
+                                print(e)
+                            time.sleep(time_wait_on_failed * (i + 1))
+                        else:
+                            failed += 1
+                        pbar.set_postfix(done=done, failed=failed)
+                        if options['dryrun']:
+                            return
 
         self.stdout.write(f'Successfully mailing: {done} of {teams.count()} ({failed} fails)')
