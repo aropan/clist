cd6fa7e21955c586c8f02b47738672b69cd51cbf
Aleksey Ropan <nap0rbl4@gmail.com>
2020-05-18 19:34:06 +0300

    Added type contest to title for atcoder

--- legacy/module/atcoder.jp/index.php
+++ legacy/module/atcoder.jp/index.php
@@ -17,7 +17,13 @@
             $page = curlexec($url);
             $url_parsed[$url] = true;
 
-            $regex = "#<tr[^>]*>(?:\s*<[^>]*>)+(?P<start_time>[^<]*)(?:<[^>]*>\s*)+(?:<span[^>]*>[^<]*</span>\s*)?<a[^>]*href=['\"](?P<url>[^\"']*/(?P<key>[^/]*))[\"'][^>]*>(?P<title>[^<]*)(?:<[^>]*>\s*)+(?P<duration>[0-9]+(?::[0-9]+)+)</td>#";
+            $regex = '#
+                <tr[^>]*>(?:\s*<[^>]*>)+(?P<start_time>[^<]*)(?:<[^>]*>\s*)+
+                (?:<span[^>]*class="(?P<class>[^"]*)"[^>]*>[^<]*</span>\s*)?
+                <a[^>]*href=[\"\'](?P<url>[^\"\']*/(?P<key>[^/]*))[\"\'][^>]*>(?P<title>[^<]*)(?:<[^>]*>\s*)+
+                (?P<duration>[0-9]+(?::[0-9]+)+)
+                </td>
+            #x';
 
             preg_match_all($regex, $page, $matches, PREG_SET_ORDER);
 
@@ -28,10 +34,28 @@
                 }
                 $seen[$k] = true;
 
+                $title = $c['title'];
+                if (!empty($c['class'])) {
+                    $classes = explode(' ', $c['class']);
+                    foreach (
+                        array(
+                            array('class' => 'user-red', 'prefix' => 'AGC', 'name' => 'AtCoder Grand Contest'),
+                            array('class' => 'user-orange', 'prefix' => 'ARC', 'name' => 'AtCoder Regular Contest'),
+                            array('class' => 'user-blue', 'prefix' => 'ABC', 'name' => 'AtCoder Beginner Contest'),
+                            array('class' => 'user-green', 'prefix' => 'ABC', 'name' => 'AtCoder Beginner Contest'),
+                        ) as $t
+                    ) {
+                        if (in_array($t['class'], $classes) && stripos($title, $t['name']) === false) {
+                            $title = $t['prefix'] . ": " . $title;
+                            break;
+                        }
+                    }
+                }
+
                 $contests[] = array(
                     'start_time' => $c['start_time'],
                     'duration' => $c['duration'],
-                    'title' => $c['title'],
+                    'title' => $title,
                     'url' => url_merge($URL, $c['url']),
                     'host' => $HOST . $host,
                     'rid' => $RID,
