65376720b9dd644fb0b5c65beda81f6871bab6c3
Aleksey Ropan <nap0rbl4@gmail.com>
2020-07-19 03:52:46 +0300

    Added binarysearch.io problem urls

--- ranking/management/modules/binarysearch.py
+++ ranking/management/modules/binarysearch.py
@@ -28,11 +28,12 @@ class Statistic(BaseModule):
         for session in room_data['sessions']:
             total = len(session['players'])
             problems_ids = []
-            for problems_set in session['questionsets']:
+            for idx, problems_set in enumerate(session['questionsets']):
                 problem = problems_set['question']
                 info = {
                     'code': str(problem['id']),
                     'name': problem['title'],
+                    'url': f'{self.url}?questionsetIndex={idx}'
                 }
                 problems_info[info['code']] = info
                 problems_ids.append(info['code'])
@@ -43,39 +44,42 @@ class Statistic(BaseModule):
                 data = json.loads(page)
                 return data
 
-            with PoolExecutor(max_workers=8) as executor:
-                n_page = (total - 1) // per_page + 1
+            if users or users is None:
+                with PoolExecutor(max_workers=8) as executor:
+                    n_page = (total - 1) // per_page + 1
 
-                rank = 0
-                idx = 0
-                last = None
-                for data in tqdm(executor.map(fetch_results, range(n_page)), total=n_page, desc='getting results'):
-                    for row in data['leaders']:
-                        idx += 1
-                        score = (row['score'], row['durationTime'])
-                        if last != score:
-                            rank = idx
-                            last = score
+                    rank = 0
+                    idx = 0
+                    last = None
+                    for data in tqdm(executor.map(fetch_results, range(n_page)), total=n_page, desc='getting results'):
+                        for row in data['leaders']:
+                            idx += 1
+                            score = (row['score'], row['durationTime'])
+                            if last != score:
+                                rank = idx
+                                last = score
 
-                        handle = row.pop('username')
-                        r = results.setdefault(handle, {})
-                        r['member'] = handle
-                        r['solving'] = row.pop('score')
-                        r['place'] = rank
-                        r['penalty'] = self.to_time(row.pop('durationTime'))
-
-                        solved = 0
-                        problems = r.setdefault('problems', {})
-                        for code, duration in zip(problems_ids, row.pop('durationTimes')):
-                            if not duration:
+                            handle = row.pop('username')
+                            if users and handle not in users:
                                 continue
-                            p = problems.setdefault(code, {})
-                            p['result'] = '+'
-                            p['time'] = self.to_time(duration)
-                            p['binary'] = True
-                            solved += 1
-                        r['solved'] = {'solving': solved}
-                        r['info'] = {'stat': row.pop('stat')}
+                            r = results.setdefault(handle, {})
+                            r['member'] = handle
+                            r['solving'] = row.pop('score')
+                            r['place'] = rank
+                            r['penalty'] = self.to_time(row.pop('durationTime'))
+
+                            solved = 0
+                            problems = r.setdefault('problems', {})
+                            for code, duration in zip(problems_ids, row.pop('durationTimes')):
+                                if not duration:
+                                    continue
+                                p = problems.setdefault(code, {})
+                                p['result'] = '+'
+                                p['time'] = self.to_time(duration)
+                                p['binary'] = True
+                                solved += 1
+                            r['solved'] = {'solving': solved}
+                            r['info'] = {'stat': row.pop('stat')}
         standings = {
             'result': results,
             'problems': list(problems_info.values()),
