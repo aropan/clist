809567907a81841b118b1fb8ffb1b65abedf675e
Aleksey Ropan <nap0rbl4@gmail.com>
2020-07-25 00:33:48 +0300

    Added submodule update

--- pyclist/wsgi.py
+++ pyclist/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pyclist.settings")  # noqa
 
+from git import Repo
 from django.core.wsgi import get_wsgi_application
 from django.core.management import call_command
 from django import setup
@@ -18,5 +19,7 @@ from django.conf import settings
 if not settings.DEBUG:
     setup()
     call_command('collectstatic', verbosity=1, interactive=False)
+    repo = Repo(os.path.join(os.path.dirname(__file__), '..'))
+    print('Submodule update:', repo.git.submodule('update', '--init'))
 
 application = get_wsgi_application()
