58fa03c70cd723850c20717722c8210cd5f8540e
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2021-03-28 19:40:47 +0300

    Removed dependabot, added python3-venv in packages

--- requirements/packages.sh
+++ requirements/packages.sh
@@ -9,7 +9,7 @@ apt upgrade -y
 apt install -y bash-completion
 apt install -y php-curl php-imap php-pspell php-tidy php-xmlrpc php-fxsl php-sqlite3 php-pgsql php-cgi php-mysql php-curl php-gd php-json php-memcache php-fpm php-mbstring php-intl
 
-apt install -y python3-pip python3-dev
+apt install -y python3-pip python3-dev python3-venv
 
 apt install -y nginx
 update-rc.d nginx enable
