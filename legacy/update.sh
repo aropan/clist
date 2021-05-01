a44ab2c1f08a8d6847d5d4345fdc4ce3cc121018
Aleksey Ropan <nap0rbl4@gmail.com>
2021-05-01 16:55:24 +0300

    Fixed python path in legacy code

--- legacy/update.sh
+++ legacy/update.sh
@@ -4,11 +4,11 @@ set -x -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-$WORKON_HOME/clist/bin/python -W ignore::DeprecationWarning api/google-calendar/common.py 2>&1
+$VIRTUAL_ENV/bin/python -W ignore::DeprecationWarning api/google-calendar/common.py 2>&1
 
 mkdir -p logs/update
 mkdir -p logs/working
 php -f update.php 2>&1 | tee logs/update/index.html
 
 mkdir -p logs/calendar
-$WORKON_HOME/clist/bin/python -W ignore::DeprecationWarning calendar/update.py 2>&1 | tee logs/calendar/index.txt
+$VIRTUAL_ENV/bin/python -W ignore::DeprecationWarning calendar/update.py 2>&1 | tee logs/calendar/index.txt
--- requirements/configure.sh
+++ requirements/configure.sh
@@ -9,7 +9,7 @@ while IFS= read -r line; do
 done <<< $(cat "$(dirname $BASH_SOURCE[0])/../pyclist/conf.py" | grep " = " | sed "s/ = '\?/=/;s/'$//")
 
 sudo -u postgres createuser $DB_USER || :
-sudo -u postgres createdb $DB_NAME || :
+sudo -u postgres createdb -E utf8 $DB_NAME || :
 
 sudo -u postgres psql -c "alter user $DB_USER with encrypted password '$DB_PASSWORD';"
 sudo -u postgres psql -c "grant all privileges on database $DB_NAME to $DB_USER;"
