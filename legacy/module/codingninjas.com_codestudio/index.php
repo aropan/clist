d67b6a314927a87b1e5ee5c8dfbf5f590871aed1
Aleksey Ropan <nap0rbl4@gmail.com>
2022-07-02 19:50:44 +0000

    Fixed codestudio

--- legacy/module/codingninjas.com_codestudio/index.php
+++ legacy/module/codingninjas.com_codestudio/index.php
@@ -1,78 +1,19 @@
 <?php
     require_once dirname(__FILE__) . '/../../config.php';
 
-    $URL = 'https://www.codingninjas.com/codestudio/contests';
-    $page = curlexec($URL);
+    $url = 'https://api.codingninjas.com/api/v3/public_section/contest_list';
+    $data = curlexec($url, null, array("json_output" => 1));
 
-    preg_match_all('#
-        <[^>]*>(?P<status>[^<]*)<[^>]*>(?:\s*<[^a][^>]*>)*
-        <h4[^>]*>(?P<title>[^<]*)</h4>\s*
-        <[^>]*>(?P<time>[^<]*)<[^>]*>(?:[^<]*<[^a][^>]*>)*
-        <a[^>]*href="(?P<url>[^"]*/contests/[^"]*)"[^>]*>
-        #x', $page, $matches, PREG_SET_ORDER);
-
-    $year = date('Y');
-    $now = time();
-    $prev = $now;
-    $month = 30 * 24 * 60 * 60;
-
-    foreach ($matches as $c) {
-        $is_past = trim(strtolower($c['status'])) == 'past';
-        $url = url_merge($URL, $c['url']);
-        $time = $c['time'];
-        if (strpos($time, ' | ') !== false) {
-            list($date, $time) = explode(' | ', $time);
-        } else {
-            $date = '';
-        }
-        if (preg_match('#\((?P<timezone>[^\)]*)\)$#', $time, $match)) {
-            $time = substr($time, 0, strlen($time) - strlen($match[0]));
-            $timezone = $match['timezone'];
-        } else {
-            $timezone = '';
-        }
-        $timezone = str_replace('CUT', 'UTC', $timezone);
-        list($start_time, $end_time) = explode(' - ', $time);
-        $start_time = trim($date . " " . $start_time . " " . $timezone);
-        $end_time = trim($date . " " . $end_time . " " . $timezone);
-
-        if (!preg_match('#/contests/(?P<key>[^/]+)#', $url, $match)) {
-            continue;
-        }
-        $key = $match['key'];
-
-        if (empty($date)) {
-            if ($is_past) {
-                $y = $year;
-                while ($y > 0 && strtotime($start_time . ', ' . $y) - 2 * $month > $prev) {
-                    $y -= 1;
-                }
-            } else {
-                $y = $year;
-                while ($y < 5000 && strtotime($start_time . ', ' . $y) < $now) {
-                    $y += 1;
-                }
-            }
-            if ($y == 0 || $y == 5000) {
-                continue;
-            }
-            $start_time .= ', ' . $y;
-            while (strtotime($end_time. ', ' . $y) < strtotime($start_time)) {
-                $y += 1;
-            }
-            $end_time .= ', ' . $y;
-        }
-        $prev = strtotime($start_time);
-
+    foreach ($data['data']['events'] as $c) {
         $contests[] = array(
-            'start_time' => $start_time,
-            'end_time' => $end_time,
-            'title' => $c['title'],
-            'url' => $url,
+            'start_time' => $c['event_start_time'],
+            'end_time' => $c['event_end_time'],
+            'title' => $c['name'],
+            'url' => "$URL/${c['slug']}",
             'host' => $HOST,
             'rid' => $RID,
             'timezone' => $TIMEZONE,
-            'key' => $key,
+            'key' => $c['slug'],
         );
     }
 ?>
