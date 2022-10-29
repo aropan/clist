edb23fa98f3b38da8955e97f5b4b6a95672045a6
Aleksey Ropan <nap0rbl4@gmail.com>
2022-10-29 20:52:49 +0000

    Resource top versus button

--- src/ranking/views.py
+++ src/ranking/views.py
@@ -1718,6 +1718,8 @@ def make_versus(request):
         whos.append(who)
 
     url = reverse('ranking:versus', args=['/vs/'.join(whos)]) if len(whos) > 1 else None
+    if request.GET.get('redirect') in settings.YES_:
+        return redirect(url)
 
     context = {
         'url': url,
--- src/static/js/resources-top.js
+++ src/static/js/resources-top.js
@@ -1,27 +1,39 @@
 function resources_top_setup_hover() {
-  $(".to-hover[data-coder]")
-    .hover(
-      function() {
-        var coder = $(this).attr('data-coder')
-        $("[data-coder='" + coder + "']").addClass('hover')
-      },
-      function() {
-        var coder = $(this).attr('data-coder')
-        $("[data-coder='" + coder + "']").removeClass('hover')
-      },
-    )
-    .click(
-      function() {
-        var coder = $(this).attr('data-coder')
+  $(".to-hover[data-coder]").hover(
+    function() {
+      var coder = $(this).attr('data-coder')
+      $("[data-coder='" + coder + "']").addClass('hover')
+    },
+    function() {
+      var coder = $(this).attr('data-coder')
+      $("[data-coder='" + coder + "']").removeClass('hover')
+    },
+  )
+  $(".to-hover").click(
+    function() {
+      var coder = $(this).attr('data-coder')
+      if (coder) {
         var elements = $("[data-coder='" + coder + "']")
         if ($(this).hasClass('fixed')) {
           elements.removeClass('fixed')
         } else {
           elements.addClass('fixed')
         }
+      } else {
+        $(this).toggleClass('fixed')
       }
-    )
-    .removeClass('to-hover')
+
+      var coders = $('.fixed[data-coder]').map((_, el) => { pk = $(el).attr('data-coder'); return "coder_" + pk + "=" + pk })
+      coders = [...new Set(coders)]
+      var accounts = $('.fixed[data-account]').map((_, el) => { pk = $(el).attr('data-account'); return "account_" + pk + "=" + pk })
+      accounts = [...new Set(accounts)]
+
+      query = coders.concat(accounts)
+      url = versus_url + '?redirect=&' + query.join('&')
+      $('#versus-fixed').attr('disabled', query.length < 2).attr('href', url)
+    }
+  )
+  $(".to-hover").removeClass('to-hover')
 }
 
 $(resources_top_setup_hover)
