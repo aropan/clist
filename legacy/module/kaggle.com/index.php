92dab89b04d3bb7c24fbbe34fc981bb95232b002
Aleksey Ropan <nap0rbl4@gmail.com>
2022-11-02 01:09:49 +0000

    Fixed kaggle parser

--- legacy/module/kaggle.com/index.php
+++ legacy/module/kaggle.com/index.php
@@ -1,62 +1,70 @@
 <?php
     require_once dirname(__FILE__) . "/../../config.php";
 
-    $add_full_list = isset($_GET['parse_full_list']);
-    $urls = array("https://www.kaggle.com/competitions.json?");
-    if (!$add_full_list) {
-        $urls[] = "https://www.kaggle.com/competitions.json?sortBy=relevance&";
+    $parse_full_list = isset($_GET['parse_full_list']);
+
+    $page = curlexec($URL);
+    if (!preg_match('#XSRF-TOKEN=(?P<token>[^;]*)#', $page, $match)) {
+        trigger_error("Not found xsrf token", E_USER_WARNING);
+        return;
     }
+    $token = $match['token'];
+
+    $url = 'https://www.kaggle.com/api/i/competitions.CompetitionService/ListCompetitions';
+    $params = array(
+        'json_output' => true,
+        'http_header' => array(
+            'content-type: application/json',
+            'x-xsrf-token: ' . $token,
+        ),
+    );
     $added = array();
-    foreach ($urls as $base_url) {
-        for ($page = 1, $n_competition = -1; $page == 1 || ($n_competition && $add_full_list); ++$page) {
-            $url = $base_url . "page=$page";
-            $data = curlexec($url, NULL, array('json_output' => true));
-            if (is_string($data) && strpos($data, '429 Too Many Requests') !== false) {
-                trigger_error("Too many requests for parse kaggle.com", E_USER_WARNING);
-                return;
-            }
-            if (!isset($data['fullCompetitionGroups'])) {
-                trigger_error("Not found competition groups", E_USER_WARNING);
-                return;
-            }
-            $list_of_competitions_list = array_merge(
-                $data['fullCompetitionGroups'],
-                array($data['pagedCompetitionGroup'])
-            );
+    foreach (
+        array(
+            'LIST_OPTION_ACTIVE',
+            'LIST_OPTION_DEFAULT',
+            'LIST_OPTION_COMPLETED',
+            'LIST_OPTION_DEFAULT',
+        ) as $list_option
+    ) {
+        $page_token = '';
+        do {
+            $post = '{"selector":{"competitionIds":[],"listOption":"' . $list_option . '","sortOption":"SORT_OPTION_NEWEST","hostSegmentIdFilter":0,"searchQuery":"","prestigeFilter":"PRESTIGE_FILTER_UNSPECIFIED","participationFilter":"PARTICIPATION_FILTER_UNSPECIFIED","tagIds":[],"requireSimulations":false},"pageToken":"' . $page_token . '","pageSize":50,"readMask":"competitions"}';
+            $data = curlexec($url, $post, $params);
 
-            $n_competition = 0;
-            foreach ($list_of_competitions_list as $group_competitions) {
-                $n_competition += count($group_competitions['competitions']);
-                foreach ($group_competitions['competitions'] as $c) {
-                    $ok = true;
-                    foreach (array('competitionId', 'competitionTitle', 'competitionUrl') as $f) {
-                        if (!isset($c[$f])) {
-                            trigger_error("Not found ${f} field", E_USER_WARNING);
-                            $ok = false;
-                            break;
-                        }
+            foreach ($data['competitions'] as $c) {
+                $ok = true;
+                foreach (array('id', 'title', 'competitionName', 'dateEnabled', 'deadline') as $f) {
+                    if (!isset($c[$f])) {
+                        trigger_error("Not found ${f} field", E_USER_WARNING);
+                        $ok = false;
+                        break;
                     }
-                    $id = $c['competitionId'];
-                    if (!$ok || (isset($c['isPrivate']) && $c['isPrivate']) || isset($added[$id])) {
-                        continue;
-                    }
-                    $added[$id] = true;
-
-                    // echo $c['deadline'] . " " . $c['competitionTitle'] . " " . $id . "\n";
-
-                    $contests[] = array(
-                        'start_time' => $c['enabledDate'],
-                        'end_time' => $c['deadline'],
-                        'title' => $c['competitionTitle'],
-                        'url' => url_merge($URL, $c['competitionUrl']),
-                        'host' => $HOST,
-                        'rid' => $RID,
-                        'timezone' => $TIMEZONE,
-                        'key' => $id
-                    );
                 }
+                if (!$ok || (isset($c['isPrivate']) && $c['isPrivate']) || isset($added[$c['id']])) {
+                    continue;
+                }
+                $added[$c['id']] = true;
+
+                $title = $c['title'];
+                if (isset($c['categories']) && is_array($c['categories'])) {
+                    $categories = array_map(function($cat) { return $cat['name']; }, $c['categories']);
+                    $title .= ' [' . implode(', ', $categories) . ']';
+                }
+
+                $contests[] = array(
+                    'start_time' => $c['dateEnabled'],
+                    'end_time' => $c['deadline'],
+                    'title' => $title,
+                    'url' => url_merge($URL, $c['competitionName']),
+                    'host' => $HOST,
+                    'rid' => $RID,
+                    'timezone' => $TIMEZONE,
+                    'key' => $c['id'],
+                );
             }
-        }
+            $page_token = isset($data['nextPageToken'])? $data['nextPageToken'] : false;
+        } while ($page_token && $parse_full_list);
     }
 
     if ($RID === -1) {
